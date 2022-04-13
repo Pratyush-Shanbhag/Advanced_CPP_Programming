d864bcb2326969a4b86e95dbb740dff7ef67bcb4
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-04-12 19:44:17 -0700

    prototyping for enigma decryption

---
+++ Lab_0/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void b() {
+    string str = "DFFCXMKGLXQKM ROSRK";
+    char a[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
+                'Z', ' '};
+    for(int i = 0; i < str.size()-3; i++) {
+        //if(str[i])
+    }
+}
+int main() {
+    
+    return 0;
+}
