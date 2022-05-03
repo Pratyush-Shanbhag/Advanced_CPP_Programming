94ce3cc07c797cdc8a502ea2a56d256a69d791e4
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-05-02 20:12:29 -0700

    created function to convert and added code in main() to convert string to bit representation

--- Lab_1/test2.cpp
+++ Lab_1/test2.cpp
@@ -4,10 +4,16 @@
 
 using namespace std;
 
-int main() {
-    unsigned char c = 'Q';
+string getBitRep(unsigned char c) {
     bitset<8> bit(c);
-    cout << bit.to_string() << endl;
+    return bit.to_string();
+}
+
+int main() {
+    string s = "QR";
+    for(int i = 0; i < s.length(); i++) {
+        cout << getBitRep(s[i]) << endl;
+    }
 
     return 0;
 }
