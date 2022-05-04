b0ab96e12de860dd0485ae1d67d8073099f69862
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-05-04 10:19:56 -0700

    tested decryption of "Q"

--- Lab_1/test2.cpp
+++ Lab_1/test2.cpp
@@ -9,11 +9,25 @@ string getBitRep(unsigned char c) {
     return bit.to_string();
 }
 
+void a() {
+    unsigned long c = 'Q';
+    bitset<8> bit(c);
+    bitset<8> as("0101");
+    bitset<8> bi("11");
+    bitset<8> result = bit >> 6 & bi;
+    cout << result << endl;
+   // bitset<2>  = result
+    cout << as.to_ulong() << endl;
+    cout << char(101) << endl;
+}
+
 int main() {
-    string s = "QR";
+    /*string s = "QR";
     for(int i = 0; i < s.length(); i++) {
         cout << getBitRep(s[i]) << endl;
-    }
+    }*/
+    
+    a();
 
     return 0;
 }
