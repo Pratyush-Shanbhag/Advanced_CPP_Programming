869144fec405dd9dde971b0580008bf9f5985dac
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-05-15 17:29:53 -0700

    added map capability and fixed errors

--- Lab_2/test_regex.cpp
+++ Lab_2/test_regex.cpp
@@ -2,23 +2,29 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <map>
 
 using namespace std;
 
 void func(string filename) {
+   map<string, double> m;
     ifstream infile;
     infile.open(filename);
     string line;
     smatch first;
     smatch second;
     regex pat1("\\('?(.)");
-    regex pat2(",(.*)(\\))");
+    regex pat2("(?!',')'*',(.*)(\\))");
     while(getline(infile, line)) {
         regex_search(line, first, pat1);
         regex_search(line, second, pat2);
-        cout << first[1] << " " << second[1] << endl;
+        m.insert(make_pair(first.str(1), stod(second.str(1))));
     }
     infile.close();
+
+    for (auto itr = m.begin(); itr != m.end(); ++itr) {
+        cout << itr->first << '\t' << itr->second << '\n';
+    }
 }
 
 int main() {
