3320ba87f7ae56432f4045687bc165e37f14a7cd
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-06-24 00:52:45 -0700

    finished

--- Ex_3_Pattern_Matching/Ex_3_Pattern_Matching.cpp
+++ Ex_3_Pattern_Matching/Ex_3_Pattern_Matching.cpp
@@ -160,6 +160,7 @@ struct Diagnostics
     Diagnostics(string s) { ncompares = 0; title = s; tbegin = system_clock::now(); }
     void operator ++ (int) { ncompares++; }
     operator int() { return ncompares; }
+    void setNCompares(int n) { ncompares = n; }
     void analysis()
     {
         tend = system_clock::now();
@@ -168,26 +169,14 @@ struct Diagnostics
     }
     void Output(string stext, int startindex)
     {
-        for (int i = 0; i < stext.length(); i++)
-        {
-            if (i < startindex)
-                cout << yellow << stext[i];
-            else
-                cout << red << stext[i];
-            this_thread::sleep_for(chrono::milliseconds(15));
-        }
+        int i;
+        for(i = 0; i < stext.length() && startindex; i++)
+            cout << yellow << stext[i];
+        for(i = startindex; i < stext.length(); i++)
+            cout << red << stext[i];
         cout << white << endl;
     }
 };
-
-
-map<char, int> generateBadCharShift(string pattern)
-{
-    map<char, int> skipList;
-    for (int i = 0; i < pattern.length() - 1; i++)
-        skipList[pattern[i]] = pattern.length() - i - 1;
-    return skipList;
-}
 
 
 int findSuffixPosition(char badchar, string suffix, string full_term)
@@ -198,38 +187,14 @@ int findSuffixPosition(char badchar, string suffix, string full_term)
         for (int suffix_index = 0; suffix_index < suffix.length(); suffix_index++)
         {
             int term_index = offset - suffix.length() - 1 + suffix_index;
-            if (term_index >= 0 && suffix[suffix_index] != 
-            full_term[term_index])
-            flag = false;
+            if (term_index >= 0 && suffix[suffix_index] != full_term[term_index])
+                flag = false;
         }
         int term_index = offset - suffix.length() - 1;
         if (flag && (term_index <= 0 || full_term[term_index - 1] != badchar))
             return full_term.length() - offset + 1;
     }
     return -1;
-}
-
-
-map<int, int> generateSuffixShift(string key)
-{
-    map<int, int> skipList;
-    string buffer = "";
-    for (int i = 0; i < key.length(); i++)
-    {
-        skipList[buffer.length()] = findSuffixPosition(key[key.length() - 1 - i], buffer, key);
-        buffer = key[key.length() - 1 - i] + buffer;
-    }
-    return skipList;
-}
-
-
-int getbadChar(char c, map<char, int>& badChar)
-{
-    auto it = badChar.find(c);
-    if (it != badChar.end())
-        return badChar[c];
-    else
-        return badChar.size() + 1;
 }
 
 
@@ -240,36 +205,53 @@ int BoyerMooreHorsepool(string text, string pattern)
     cout << yellow << "pattern: " << pattern << endl;
     cout << yellow << "target:  " << text << endl;
 
-    map<int, int> goodSuffix = generateSuffixShift(pattern);
-    for (auto g : goodSuffix)
+    map<int, int> goodSuffix = [](string key) {
+                                    map<int, int> skipList;
+                                    string buffer = "";
+                                    for (int i = 0; i < key.length(); i++)
+                                    {
+                                        skipList[buffer.length()] = findSuffixPosition(key[key.length() - 1 - i], buffer, key);
+                                        buffer = key[key.length() - 1 - i] + buffer;
+                                    }
+                                    return skipList;
+                                 }(pattern);
+    for (auto &g : goodSuffix)
         cout << "<" << g.first << "," << g.second << ">" << endl;
 
-    map<char, int> badChar = generateBadCharShift(pattern);
-    for (auto b : badChar)
+    map<char, int> badChar = [](string pattern) {
+                                    map<char, int> skipList;
+                                    for (int i = 0; i < pattern.length() - 1; i++)
+                                        skipList[pattern[i]] = pattern.length() - i - 1;
+                                    return skipList;
+                               }(pattern);
+
+    for (auto &b : badChar)
         cout << "<" << b.first << "," << b.second << ">" << endl;
 
     int i = 0;
     while (i < text.length() - pattern.length() + 1)
     {
         diag.Output(text, i);
-        int j = pattern.length();
-        while (j > 0 && pattern[j - 1] == text[i + j - 1])
-        {
-            diag++;
-            j -= 1;
-        }
+        int j = pattern.length() + 1;
+
+        while (--j > 0 && pattern[j - 1] == text[i + j - 1]) { }
+        diag.setNCompares(pattern.length() - j);
+
         if (j > 0)
         {
-            int badCharShift = getbadChar(text[i + j - 1], badChar);
+            int badCharShift = [](char c, map<char, int>& badChar) {
+                                    auto it = badChar.find(c);
+                                    if (it != badChar.end())
+                                        return badChar[c];
+                                    else
+                                        return static_cast<int>(badChar.size()) + 1;
+                                 }(text[i + j - 1], badChar);
             int goodSuffixShift = goodSuffix[pattern.length() - j];
 
             cout << cyan << "badCharShift " << badCharShift << " target[" << i + j - 1 << "] = " << text[i + j - 1] << endl;
             cout << cyan << "goodSuffixShift " << goodSuffixShift << " goodSuffix[" << pattern.length() - j << "] = " << text[goodSuffix[pattern.length() - j]] << endl;
             
-            if (badCharShift > goodSuffixShift)
-                i += badCharShift;
-            else
-                i += goodSuffixShift;
+            i += max(badCharShift, goodSuffixShift);
         }
         else
         {
